9471baa12a2e371c68bcddc33cacfafe70730ad1
PyNAABO <86821481+PyNAABO@users.noreply.github.com>
2022-04-02 15:21:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, send_file
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, PyNAABO!'
+
+@app.route('/get_image')
+def get_image():
+    if request.args.get('type') == '1':
+       filename = 'logo.webp'
+       return send_file(filename)
+    else:
+       filename = 'ERROR.gif'
+       return send_file(filename, mimetype='image/gif')
